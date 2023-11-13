79889fb6e4e8b1584808bf64261bdf03be0d95fe
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2023-11-13 23:24:17 +0100

    added formatting

--- podstawypythona.pl/lesson_01/task_02.py
+++ podstawypythona.pl/lesson_01/task_02.py
@@ -1,10 +1,10 @@
 while True:
     conversion_choice = input('Enter 1 to convert from Celsius to Fahrenheit, 2 to convert from Fahrenheit to Celsius: ')
     
-
+# ":.2f - daje 2 miejsca po przecinku"
     if conversion_choice == '1':
         degrees = float(input('Provide temperature to convert: '))
-        print((degrees * 9/5) + 32)
+        print(f'{(degrees * 9/5) + 32:.2f}')
         break
     elif conversion_choice == '2':
         degrees = float(input('Provide temperature to convert: '))
