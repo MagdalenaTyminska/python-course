7e22006fe7727f4e04fa7c7ad733b4afa8553597
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2023-11-13 23:16:48 +0100

    fixed

--- podstawypythona.pl/lesson_01/task_02.py
+++ podstawypythona.pl/lesson_01/task_02.py
@@ -1,12 +1,12 @@
 while True:
-    conversion_choice = int(input('Enter 1 to convert from Celsius to Fahrenheit, 2 to convert from Fahrenheit to Celsius: '))
+    conversion_choice = input('Enter 1 to convert from Celsius to Fahrenheit, 2 to convert from Fahrenheit to Celsius: ')
     
 
-    if conversion_choice == 1:
+    if conversion_choice == '1':
         degrees = float(input('Provide temperature to convert: '))
         print((degrees * 9/5) + 32)
         break
-    elif conversion_choice == 2:
+    elif conversion_choice == '2':
         degrees = float(input('Provide temperature to convert: '))
         print((degrees - 32) * 5/9)
         break
